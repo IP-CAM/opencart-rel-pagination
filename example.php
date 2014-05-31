ce90ae34bae725d37b7e1e525111535aafe9e751
Eugene Lifescale <eugene.lifescale@gmail.com>
2014-05-31 20:19:01 +0300

    initial commit

---
+++ example.php
@@ -0,0 +1,21 @@
+// PHP Example for OpenCart 1.5
+
+<?php 
+
+  $num_pages = ceil($product_total / $limit);
+  
+  $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . $url . '&page=' . $page), 'canonical');
+  
+  if ($page < $num_pages) {
+      $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . $url . '&page=' . ($page + 1)), 'next');
+  }
+  
+  if ($page > 1) {
+  
+      // Remove page duplicate
+      if ($page == 2) {
+          $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . $url), 'prev');
+      } else {
+          $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . $url . '&page=' . ($page - 1)), 'prev');
+      }
+  }
