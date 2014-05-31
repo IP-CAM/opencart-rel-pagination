151864c7238989979b830693a2fdb7b90edc9e3b
Eugene Lifescale <eugene.lifescale@gmail.com>
2014-05-31 21:09:24 +0300

    add code comments

--- example.php
+++ example.php
@@ -4,16 +4,19 @@
 
   $num_pages = ceil($product_total / $limit);
   
+  // Canonical
   if ($page == 1) {
       $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path']), 'canonical');
   } else {
       $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . '&page=' . $page), 'canonical');
   }
   
+  // Next
   if ($page < $num_pages) {
       $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . '&page=' . ($page + 1)), 'next');
   }
   
+  // Prev
   if ($page > 1) {
   
       if ($page == 2) {
