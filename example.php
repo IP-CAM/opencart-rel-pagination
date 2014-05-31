02bd0c69c9cc0c24d3f1304e760f217f35b1bb78
Eugene Lifescale <eugene.lifescale@gmail.com>
2014-05-31 21:08:11 +0300

    remove page=1 duplicate

--- example.php
+++ example.php
@@ -4,7 +4,11 @@
 
   $num_pages = ceil($product_total / $limit);
   
-  $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . '&page=' . $page), 'canonical');
+  if ($page == 1) {
+      $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path']), 'canonical');
+  } else {
+      $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . '&page=' . $page), 'canonical');
+  }
   
   if ($page < $num_pages) {
       $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path'] . '&page=' . ($page + 1)), 'next');
@@ -12,7 +16,6 @@
   
   if ($page > 1) {
   
-      // Remove page duplicate
       if ($page == 2) {
           $this->document->addLink($this->url->link('product/category', 'path=' . $this->request->get['path']), 'prev');
       } else {
